ca2f750a69e5b779b5c86387debfade23397af5e
Harshit Sharma <harshit.sharma.s84@kalvium.community>
2025-03-16 11:30:59 +0530

    Done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    build: {
-      rollupOptions: {
-        output: {
-          dir: "dist", // ✅ Correct option
-          entryFileNames: "index.js"
-        }
-      }
-    }
 })
+
 
